b8dbb1d902b096b5761860784bed2d453b633dff
Zerumi <evgexa.2015@bk.ru>
2022-09-07 22:53:51 +0300

    formula correction, right output - 07/09/2022 22:53

--- src/Program.java
+++ src/Program.java
@@ -18,44 +18,46 @@ public class Program {
             for (int j = 0; j < arr[i].length; j++) {
                 double X = x[j];
                 double result;
-                //System.out.println("Element " + arr[i][j] + " at: " + i + " " + j);
                 switch (c[i])
                 {
                     case 12:
                     {
                         double sa1 = (4-X)/X; // 1
                         double sa = Math.pow(X, sa1); // 2
-                        double sb = 1.0/2.0 - Math.tan(X); // 3
+                        double sb = (1.0 / 2.0) - Math.tan(X); // 3
                         double s = sa/sb; // 4
-
-                        double a1 = Math.pow((2.0/3.0)*X,3); // 5
+                        // степень основного числителя подсчитана
+                        double a1 = Math.pow((2.0 / 3.0)*X,3); // 5
                         double a = Math.pow(a1, s); // 6
+                        // основной числитель подсчитан
 
-                        double b1a = Math.asin((X + 1)/1*Math.E + 1);
-                        double b1b = 3 + b1a;
-                        double b1 = Math.pow(b1a/b1b,2);
-                        double b = 1 - b1;
+                        double b1a = Math.asin((((X + 1) / 1) * Math.E) + 1); // 7 // IDE: '(X + 1) / 1' can be replaced with '(X + 1)'
+                        double b1b = 3 + b1a; // 8
+                        // дробь в знаменателе подсчитана
+                        double b1 = Math.pow(b1a / b1b,2); // 9
+                        double b = 1 - b1; // 10
 
-                        result = Math.pow(a/b,2);
+                        result = Math.pow(a / b,2); // 11
                         break;
                     }
                     case 8:
                     case 18:
                     case 20:
                     case 22:
-                        result = Math.sin(Math.pow(Math.sin(X), 1.0/3.0));
+                        result = Math.sin(Math.cbrt(Math.sin(X)));
                         break;
                     default:
                     {
                         double a = 3.0;
-                        double bs1 = Math.atan((X + 1)/1*Math.E + 1);
-                        double bs = Math.pow(bs1, 1.0/3.0);
+                        double bs1 = Math.atan((((X + 1) / 1) * Math.E) + 1); // IDE: '(X + 1) / 1' can be replaced with '(X + 1)'
+                        double bs = Math.cbrt(bs1);
                         double b = Math.pow(Math.E, bs);
 
-                        double s1 = Math.sin(Math.pow(X + 1.0/3.0, 3)-1);
-                        double s = s1/2 /3;
+                        double s1 = Math.sin(Math.pow(X + 1.0 / 3.0, 3)) - 1;
+                        double s = (s1 / 2) / 3;
 
-                        result = Math.pow(a/b, s);
+                        result = Math.pow(a / b, s);
+                        break;
                     }
                 }
                 arr[i][j] = result;
@@ -65,8 +67,10 @@ public class Program {
         // step 4
         for (double[] doubles : arr) {
             for (double aDouble : doubles) {
-                System.out.printf("%.5g%n%n", aDouble);
+                //System.out.println("Element " + arr[i][j] + " at: " + i + " " + j);
+                System.out.printf("%.4g  ", aDouble);
             }
+            System.out.println();
         }
     }
 }
